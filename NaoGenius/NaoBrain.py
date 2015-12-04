c5eadc7b9a6218ae2f0a649af4985b8aa9e696af
Arthur Rousseau <arthur.rousseau2@gmail.com>
2015-12-04 20:02:58 +0100

    Added content to NaoBrain

--- NaoGenius/NaoBrain.py
+++ NaoGenius/NaoBrain.py
@@ -15,6 +15,12 @@ class NaoBrain():
 		self._letters = {}
 
 	def add_word(self, word) :
+		"""
+		Adds an object to the brain dictionary.
+
+		Args:
+			word: the object to be added to the dictionary.
+		"""
 		if sys.version_info[0] > 2 :
 			from unidecode import unidecode
 			temp = unidecode(word)
@@ -24,6 +30,9 @@ class NaoBrain():
 		self.dictionary[temp] = temp
 
 	def most_common_letters(self) :
+		"""
+		Return a list of the most commonly used letters.
+		"""
 		i = int(0)
 		for word in self.dictionary :
 			for letter in word.lower() :
@@ -34,6 +43,12 @@ class NaoBrain():
 			i = i + 1
 			
 	def load_file(self, file) :
+		"""
+		Load the content of a file and add each file line to the brain dictionary.
+
+		Args:
+			file: file to load.
+		"""
 		words = FileAccess.load_file(file)
 		i = int(0)
 		for word in words :
@@ -41,6 +56,12 @@ class NaoBrain():
 			i = i + 1
 
 	def save_file(self, file) :
+		"""
+		Save the dictionary content to a file.
+
+		Args:
+			file: file where the dictionary content will be save.
+		"""
 		file_content = ""
 		for word in self.dictionary :
 			file_content += word + "\n"
