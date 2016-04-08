c6fbb9ad5e45888484b580d1a276f5e30f2b6a55
Arthur ROUSSEAU <arthur.rousseau2@gmail.com>
2016-04-08 14:17:24 +0200

    Changed entry point functions

--- NaoGenius/Program.py
+++ NaoGenius/Program.py
@@ -1,14 +1,12 @@
 # -*- coding: utf-8 -*-
+
 import sys
-from NaoBrain import NaoBrain
 
 version = (2,7)
 if sys.version_info < version :
     print("Python >= {0}.{1} is required to launch this program".format(version[0], version[1]))
     sys.exit(1)
 
-brain = NaoBrain()
-brain.load_file("resources/dictionary.txt")
-
-brain.most_common_letters()
-print(brain.letters)
+import random
+LETTERS = list("abcdefghijklmnopqrstuvwxyz-")
+print(random.sample(LETTERS, 1))
--- NaoGenius/main.py
+++ NaoGenius/main.py
@@ -1,37 +1,20 @@
 # -*- coding: utf-8 -*-
 import sys
-from NaoGenius import NaoGenius
+from HumanNovice import HumanNovice
+from HumanGenius import HumanGenius
+from ArtificialNovice import ArtificialNovice
+from ArtificialGenius import ArtificialGenius
 from HangmanGame import HangmanGame
-from drawn.DrawHangman import DrawHangman
+
 
 def main():
-    nao  = NaoGenius()
-    word = nao.pick_up_a_word()
-    game = HangmanGame(word)
-    drawHangman = DrawHangman()
+	novice = ArtificialNovice()
+	genius = ArtificialGenius()
 
-    while not (game.is_winner()) and not (game.is_over()):
-        if sys.version_info[0] > 2 :
-            letter = input("Donne moi une lettre :")
-        else : 
-            letter = raw_input("Donne moi une lettre : ")
+	game = HangmanGame(genius, novice)
 
-        if letter in word and letter not in game.letter_checked:
-            print("Cette lettre appartient bien au mot")
-            game.complete_word(letter)
-            game.letter_checked.append(letter)
-
-        else:
-        	game.errors += 1
-        	drawHangman.drawn_hangman(game.errors)
-        	print("Cette lettre n'est pas bonne ou a deja ete donnee")
-
-        game.display_incomplete_word()
-
-    if game.is_winner() :
-        print("Tu as gagne")
-    else :
-        print("Tu es pendu : le mot etait {0}".format(word))
+	while (True):
+		game.launch()
 
 if __name__ == "__main__":
-    main()
+	main()
